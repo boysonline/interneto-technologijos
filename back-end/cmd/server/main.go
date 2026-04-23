0a8c94809eaeb6213ccf0d8275b74de174e56229
boysonline <rmstrazdas@gmail.com>
2026-04-23 21:17:37 +0300

    mongodb.go

--- back-end/cmd/server/main.go
+++ back-end/cmd/server/main.go
@@ -2,27 +2,12 @@ package main
 
 import (
 	"back-end/internal/database"
-	"back-end/internal/model"
-	"time"
 )
 
 func main() {
 
 	database.Connect()
 
-	// Example: Create a test lecture
-	newLecture := model.Lecture{
-		CalendarEvent: model.CalendarEvent{
-			Title:     "TEST LECTURE",
-			StartTime: time.Now(),
-			EndTime:   time.Now().Add(time.Hour * 2),
-		},
-		Type:       "Lecture",
-		Auditorium: "TEST 0",
-	}
-
-	// Save it!
-	database.CreateLecture(newLecture)
 }
 
 // myliu tave
--- back-end/internal/repository/repository.go
+++ back-end/internal/repository/repository.go
@@ -1,1 +1,22 @@
 package repository
+
+import (
+	"back-end/internal/model"
+	"context"
+
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type Repository struct {
+	Collection *mongo.Collection
+}
+
+func (repository *Repository) CreateLecture(lecture model.Lecture) error {
+	_, err := repository.Collection.InsertOne(context.TODO(), lecture)
+	return err
+}
+
+func (repository *Repository) CreateAssignment(assignment model.Assignment) error {
+	_, err := repository.Collection.InsertOne(context.TODO(), assignment)
+	return err
+}
